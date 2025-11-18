e0751bf698e76a1715d6f91c5d8b6839b51e6ccd
curryy77 <michalcolesnik@gmail.com>
2025-11-18 18:43:26 +0300

    Added the function of cleaning gnomAD DMD Database in the data_preparation.py

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -65,3 +65,42 @@ def load_and_clean_clinvar(path: str | Path) -> pd.DataFrame:
 
     df = df[cols]
     return df
+
+# Working with gnomAD DMD Database
+def load_and_clean_gnomad(path: str | Path) -> pd.DataFrame:
+    df = pd.read_csv(path)
+
+    df = df.rename(columns= {
+        "Chromosome": "chr38",
+        "Position": "pos38",
+        "Reference": "ref",
+        "Alternate": "alt",
+        "Allele Frequency": "allele_freq",
+        "ClinVar Variation ID": "var_id",
+        "Hemizygote Count": "hemizygote_count",
+        "Homozygote Count": "homozygote_count",
+    })
+
+    # Turning the genomic position in the numeric type, same as in ClinVar
+    df["pos38"] = pd.to_numeric(df["pos38"], errors="coerce")
+
+    # Returns true if the allele frequence is not NaN
+    df["in_gnomad"] = ~df["allele_freq"].isna()
+
+    # Checks if the allele frequency is high enough
+    df["af_high"] = df["allele_freq"] > 1e-4
+
+    cols = [
+        "var_id",
+        "chr38",
+        "ref",
+        "alt",
+        "allele_freq",
+        "in_gnomad",
+        "af_high",
+        "hemizygote_count",
+        "homozygote_count",
+    ]
+
+    df = df[cols]
+    return df
